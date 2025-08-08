c09836d2fae1d8084f72be2433f178ef89dcd9ed
Ivan E. Cao-Berg <icaoberg@gmail.com>
2025-08-08 08:21:15 -0400

    Updated rnaview

--- 2.0.0/rbuild.sh
+++ 2.0.0/rbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright © 2021 Pittsburgh Supercomputing Center.
+# Copyright © 2021-2025 Pittsburgh Supercomputing Center.
 # All Rights Reserved.
 
 IMAGE=singularity-rnaview-latest.sif
